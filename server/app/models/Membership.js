68f5f4ae809e3eabddcee212cdd603a28739f06a
Anupam Arya <anu9am@gmail.com>
2022-03-23 03:41:52 +0530

    models for book, exchange, membership, society,user

--- server/app/models/Membership.js
+++ server/app/models/Membership.js
@@ -4,8 +4,11 @@ const membership = mongoose.Schema(
     {
         // membership_id: unique > required > ObjectId > random / auto
         // user_id: ObjectId > ref: users
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
         // membership_date: date > required > "DD/MM/YYYY"
+        membership_date: { type: Date, required: true },
         // expiry: date > required > "DD/MM/YYYY" > (for now) membership_date +1 year if transaction_details[0] is successful
+        expiry: { type: Date, required: true },
         // transaction_details: array > [
         //                                 {
         //                                     transaction_id: string > required > unique > random / auto
@@ -14,6 +17,12 @@ const membership = mongoose.Schema(
         //                                     reference_code: string > required > "generated from stripe"
         //                                 }
         //                             ]
+        transaction_details: [{
+            transaction_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+            amount: { type: Number, required: true },
+            status: { type: String, required: true },
+            reference_code: { type: String, required: true },
+        }]
     },
     { timestamps: true }
 );
