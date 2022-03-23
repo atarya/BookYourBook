4d0527acd9a9e3475c8dd7c47d91da17103c9e08
vhald <vaibhavgsh@gmail.com>
2022-03-23 16:36:15 +0530

    routes added for new page

---
+++ client_nativeV/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+// screens
+import Splash from './screens/Splash';
+import Login from './screens/Login';
+import MobVerify from './screens/MobVerify';
+import SignUp from './screens/Signup';
+import Welcome from './screens/Welcome';
+
+export default function App() {
+  return (
+
+    <MobVerify />
+    // <View style={styles.container}>
+    //   {/* below line was for hello world and button demo */}
+    //    {/* <Text>Hello world!</Text>
+    //   <Button title="Alerty" onPress={()=> alert("Kya haal chaal")}/> */}
+    //   <StatusBar style="auto" />
+    // </View>
+
+  );
+}
+
+
