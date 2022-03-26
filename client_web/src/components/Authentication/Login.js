a65ee3b6141708fd3d605a11099ca56b8be7bec7
Anupam Arya <anu9am@gmail.com>
2022-03-27 04:14:39 +0530

    added Login form

--- client_web/src/components/Authentication/Login.js
+++ client_web/src/components/Authentication/Login.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { VStack, Button } from '@chakra-ui/react'
+import { FormControl, FormLabel } from '@chakra-ui/form-control'
+import { Input, InputGroup, InputRightElement } from '@chakra-ui/input'
 
 const Login = () => {
-         return (
-                  <div>Login</div>
-         )
+	const [phone, setPhone] = useState('')
+	const [password, setPassword] = useState('')
+	const [show, setShow] = useState(true)
+
+	const handleClick = () => {
+		setShow(!show)
+	}
+
+	const submitHandler = (e) => {
+
+	}
+
+	return (
+		<VStack spacing="5px" color="black">
+			<FormControl id="phone" isRequired>
+				<FormLabel>Phone</FormLabel>
+				<Input placeHolder="Enter your Number" onChange={(e) => { setPhone(e.target.value) }} />
+			</FormControl>
+			<FormControl id="password" isRequired>
+				<FormLabel>Password</FormLabel>
+				<InputGroup>
+					<Input type={show ? "password" : "text"} placeHolder="Enter your Password" onChange={(e) => { setPassword(e.target.value) }} />
+					<InputRightElement width="4.5rem">
+						<Button h="1.75rem" size="sm" onClick={handleClick}>{show ? "Show" : "Hide"}</Button>
+					</InputRightElement>
+				</InputGroup>
+			</FormControl>
+			<Button
+				colorScheme="teal"
+				variant="outline"
+				width="100%"
+				style={{ marginTop: 15 }}
+				onClick={() => { submitHandler() }}
+			>
+				Login
+			</Button>
+		</VStack>
+	)
 }
 
 export default Login
--- client_web/src/components/Authentication/Signup.js
+++ client_web/src/components/Authentication/Signup.js
@@ -1,80 +1,80 @@
 import React, { useState } from 'react'
-import { VStack, Button, Box, Menu, MenuButton, MenuList, MenuItem, MenuGroup, MenuDivider } from '@chakra-ui/react'
+import { VStack, Button, Box, Menu, MenuButton, MenuList, MenuItem, MenuGroup } from '@chakra-ui/react'
 import { FormControl, FormLabel } from '@chakra-ui/form-control'
 import { Input, InputGroup, InputRightElement } from '@chakra-ui/input'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const Signup = () => {
-         const [name, setName] = useState('')
-         const [phone, setPhone] = useState('')
-         const [password, setPassword] = useState('')
-         const [show, setShow] = useState("Show")
-         const [startDate, setStartDate] = useState(new Date());
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [password, setPassword] = useState('')
+    const [show, setShow] = useState(true)
+    const [startDate, setStartDate] = useState(new Date());
 
-         const handleClick = () => {
-                  setShow(!show)
-         }
+    const handleClick = () => {
+        setShow(!show)
+    }
 
-         const postDetails = (avatar) => {
+    const postDetails = (avatar) => {
 
-         }
+    }
 
-         const submitHandler = (e) => {
+    const submitHandler = (e) => {
 
-         }
+    }
 
-         return (
-                  <VStack spacing="5px" color="black">
-                           <FormControl id="name" isRequired>
-                                    <FormLabel>Name</FormLabel>
-                                    <Input placeHolder="Enter your Name" onChange={(e) => { setName(e.target.value) }} />
-                           </FormControl>
-                           <FormControl id="phone" isRequired>
-                                    <FormLabel>Phone</FormLabel>
-                                    <Input placeHolder="Enter your Number" onChange={(e) => { setPhone(e.target.value) }} />
-                           </FormControl>
-                           <FormControl id="password" isRequired>
-                                    <FormLabel>Password</FormLabel>
-                                    <InputGroup>
-                                             <Input type={show ? "password" : "text"} placeHolder="Enter your Password" onChange={(e) => { setPassword(e.target.value) }} />
-                                             <InputRightElement width="4.5rem">
-                                                      <Button h="1.75rem" size="sm" onClick={handleClick}>{show ? "Show" : "Hide"}</Button>
-                                             </InputRightElement>
-                                    </InputGroup>
-                           </FormControl>
-                           <FormControl id="date" isRequired>
-                                    <Box d="flex" flexDirection="row">
-                                             <FormLabel>DOB</FormLabel>
-                                             <DatePicker style={{ width: "50%" }} selected={new Date()} style={{ marginTop: 15 }} onChange={(date: Date) => setStartDate(date)} />
-                                             <Menu w="50%">
-                                                      <MenuButton as={Button} colorScheme='pink'>
+    return (
+        <VStack spacing="5px" color="black">
+            <FormControl id="name" isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input placeHolder="Enter your Name" onChange={(e) => { setName(e.target.value) }} />
+            </FormControl>
+            <FormControl id="phone" isRequired>
+                <FormLabel>Phone</FormLabel>
+                <Input placeHolder="Enter your Number" onChange={(e) => { setPhone(e.target.value) }} />
+            </FormControl>
+            <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                    <Input type={show ? "password" : "text"} placeHolder="Enter your Password" onChange={(e) => { setPassword(e.target.value) }} />
+                    <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" onClick={handleClick}>{show ? "Show" : "Hide"}</Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+            <FormControl id="date" isRequired>
+                <Box d="flex" flexDirection="row">
+                    <FormLabel>DOB</FormLabel>
+                    <DatePicker style={{ width: "50%" }} selected={new Date()} style={{ marginTop: 15 }} onChange={(date: Date) => setStartDate(date)} />
+                    <Menu w="50%">
+                        <MenuButton as={Button} colorScheme='pink'>
 
-                                                      </MenuButton>
-                                                      <MenuList>
-                                                               <MenuGroup title='Gender'>
-                                                                        <MenuItem>Male</MenuItem>
-                                                                        <MenuItem>Female </MenuItem>
-                                                               </MenuGroup>
-                                                      </MenuList>
-                                             </Menu>
-                                    </Box>
-                           </FormControl>
-                           <FormControl id="avatar" isRequired>
-                                    <FormLabel>Upload Avatar</FormLabel>
-                                    <Input type="file" p={1.5} accept="image/*" onChange={(e) => { postDetails(e.target.files[0]) }} />
-                           </FormControl>
-                           <Button
-                                    colorScheme="teal"
-                                    variant="outline"
-                                    width="100%"
-                                    style={{ marginTop: 15 }}
-                                    onClick={() => { submitHandler() }}
-                           >
-                                    SignUp
-                           </Button>
-                  </VStack>
-         )
+                        </MenuButton>
+                        <MenuList>
+                            <MenuGroup title='Gender'>
+                                <MenuItem>Male</MenuItem>
+                                <MenuItem>Female </MenuItem>
+                            </MenuGroup>
+                        </MenuList>
+                    </Menu>
+                </Box>
+            </FormControl>
+            <FormControl id="avatar" isRequired>
+                <FormLabel>Upload Avatar</FormLabel>
+                <Input type="file" p={1.5} accept="image/*" onChange={(e) => { postDetails(e.target.files[0]) }} />
+            </FormControl>
+            <Button
+                colorScheme="teal"
+                variant="outline"
+                width="100%"
+                style={{ marginTop: 15 }}
+                onClick={() => { submitHandler() }}
+            >
+                SignUp
+            </Button>
+        </VStack>
+    )
 }
 
 export default Signup
