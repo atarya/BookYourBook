a2456262558b7f9f58575608b2735a8ba52779a2
vhald <vaibhavgsh@gmail.com>
2022-03-27 23:39:55 +0530

    homepages added

---
+++ client_nativeV/src/Screens/Home/index.js
@@ -0,0 +1,17 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import styles from './styles';
+
+
+// create a component
+const Home = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Home</Text>
+        </View>
+    );
+};
+
+
+export default Home;
