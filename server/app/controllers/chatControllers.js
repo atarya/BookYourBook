25ea82c6b85f1296fc89fce4633589242ec1fe00
Anupam Arya <anu9am@gmail.com>
2022-03-28 13:55:51 +0530

    accessChat Controller for 1 to 1 chat

---
+++ server/app/controllers/chatControllers.js
@@ -0,0 +1,77 @@
+const asyncHandler = require("express-async-handler")
+const Chat = require("../models/Chat")
+const User = require("../models/User")
+
+const accessChat = asyncHandler(async (req, res) => {
+    const { userId } = req.body
+
+    if (!userId) {
+        res.status(400);
+        throw new Error("UserId param not sent!");
+    }
+
+    var isChat = await Chat.find({
+        isGroupChat: false,
+        $and:
+            [
+                { users: { $elemMatch: { $eq: req.user._id } } },
+                { users: { $elemMatch: { $eq: userId } } },
+            ]
+    })
+        .populate("users", "-password")
+        .populate("latestMessage");
+
+    isChat = await User.populate(isChat, {
+        path: "latestMessage.sender",
+        select: "name, avatar, phone"
+    });
+
+    if (isChat.length > 0) {
+        res.send(isChat[0]);
+    } else {
+
+        var chatData = {
+            chatName: "sender",
+            isGroupChat: false,
+            users: [req.user._id, userId],
+        }
+
+        try {
+
+            const createdChat = await Chat.create(chatData);
+            const FullChat = await Chat.findOne({ _id: createdChat._id })
+                .populate("users", "-password")
+
+            res.status(200).send(FullChat);
+
+        } catch (error) {
+
+            res.status(400);
+
+            throw new Error(error.message);
+
+        }
+    }
+})
+
+const fetchChats = asyncHandler(async (req, res) => {
+
+})
+
+const createGroupChat = asyncHandler(async (req, res) => {
+
+})
+
+const renameGroup = asyncHandler(async (req, res) => {
+
+})
+
+const removeFromGroup = asyncHandler(async (req, res) => {
+
+})
+
+const addToGroup = asyncHandler(async (req, res) => {
+
+})
+
+module.exports = { accessChat, fetchChats, createGroupChat, renameGroup, removeFromGroup, addToGroup }
