4f085f3b5656cc68f09fb639487e4ab0aa3b2397
vhald <vaibhavgsh@gmail.com>
2022-03-28 23:47:00 +0530

    routes updated

--- client_nativeV/src/Navigation/Routes.js
+++ client_nativeV/src/Navigation/Routes.js
@@ -3,23 +3,25 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { Home, Profile } from '../Screens';
-
-// ../Screens/index is same as /Screens by default it runs index
+import {
+    Home,
+    Profile,
+    Explore,
+} from '../Screens';
+import navigationStrings from '../constants/navigationStrings';
 
 const Stack = createStackNavigator();
 
 function Routes() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Profile'>
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Profile" component={Profile} />
+            <Stack.Navigator initialRouteName={navigationStrings.HOME} screenOptions={{ headerShown: false }}>
+                <Stack.Screen options={{ title: 'Book Your Book' }} name={navigationStrings.HOME} component={Home} />
+                <Stack.Screen name={navigationStrings.PROFILE} component={Profile} />
+                <Stack.Screen name={navigationStrings.EXPLORE} component={Explore} />
             </Stack.Navigator>
-        </NavigationContainer>
+        </NavigationContainer >
     )
 }
 
 export default Routes;
-
-{/*  */ }
