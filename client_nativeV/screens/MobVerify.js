351d9457b01191c4e357cfec250aa33aeac5f5f2
vhald <vaibhavgsh@gmail.com>
2022-04-02 18:13:04 +0530

    updated OTP screen (in progress)

--- client_nativeV/screens/MobVerify.js
+++ client_nativeV/screens/MobVerify.js
@@ -34,73 +34,47 @@ import { Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
 // colors
 const { brand, darkLight, primary } = Colors;
 
-// DateTime Picker
-import DateTimePicker from '@react-native-community/datetimepicker';
 
-const SignUp = () => {
-    const [hidePassword, setHidePassword] = useState(true);
-    const [show, setShow] = useState(false);
-    const [date, setDate] = useState(new Date(2000, 0, 1));
 
-    // actual date of birth to be sent
-    const [dob, setDob] = useState();
-
-    const onChange = (event, selectedDate) => {
-        const currentDate = selectedDate || date;
-        setShow(false);
-        setDate(currentDate);
-        setDob(currentDate);
-    };
-
-    const showDatePicker = () => {
-        setShow(true);
-    };
-
+const MobVerify = () => {
+    // const [hidePassword, setHidePassword] = useState(true);
+    // const [show, setShow] = useState(false);
     return (
         <StyledContainer>
             <StatusBar style="dark" />
             <InnerContainer>
-                <PageTitle>Verify Yourself</PageTitle>
-                <SubTitle>Mobile Verification</SubTitle>
-
-                {show && (
-                    <DateTimePicker testID="dateTimePicker" value={date} mode='date' is24Hour={true} display="default" onChange={onChange} />
-                )}
+                <PageTitle>Mobile Verification</PageTitle>
+                <SubTitle>Code is sent to \MOBILE NO.\</SubTitle>
 
                 <Formik
-                    initialValues={{ fullName: '', email: '', dateOfBirth: '', password: '', confirmPassword: '' }}
+                    initialValues={{ OTP: '' }}
                     onSubmit={(values) => {
                         console.log(values);
                     }}
                 >
                     {({ handleChange, handleBlur, handleSubmit, values }) => (
                         <StyledFormArea>
-
-
                             <MyTextInput
-                                label="OTP"
-                                icon="lock"
-                                placeholder="* * * * *"
-                                placeholderTextColor={darkLight}
-                                onChangeText={handleChange('password')}
-                                onBlur={handleBlur('password')}
-                                value={values.password}
-                                secureTextEntry={hidePassword}
-                                isPassword={true}
-                                hidePassword={hidePassword}
-                                setHidePassword={setHidePassword}
+                                label="Enter OTP"
+                                icon="key"
+                                placeholder="Enter your 6 digit OTP"
+                                placeholderTextColor={'#777'}
+                                onChangeText={handleChange('OTP')}
+                                onBlur={handleBlur('OTP')}
+                                value={values.OTP}
+                                keyboardType="phone-pad"
                             />
 
 
                             <MsgBox>...</MsgBox>
                             <StyledButton onPress={handleSubmit}>
-                                <ButtonText>Verify OTP</ButtonText>
+                                <ButtonText>Verify and Create Account</ButtonText>
                             </StyledButton>
                             <Line />
                             <ExtraView>
-
+                                <ExtraText>Didn't recieved code? </ExtraText>
                                 <TextLink>
-                                    <TextLinkContent>Resend OTP</TextLinkContent>
+                                    <TextLinkContent>Request Again in 20 sec</TextLinkContent>
                                 </TextLink>
                             </ExtraView>
                         </StyledFormArea>
@@ -111,7 +85,7 @@ const SignUp = () => {
     );
 };
 
-const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, isDate, showDatePicker, ...props }) => {
+const MyTextInput = ({ label, icon, isDate, ...props }) => {
     return (
         <View>
             <LeftIcon>
@@ -119,23 +93,9 @@ const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, i
             </LeftIcon>
             <StyledInputLabel>{label}</StyledInputLabel>
             {!isDate && <StyledTextInput {...props} />}
-            {isDate && (
-                <TouchableOpacity onPress={showDatePicker}>
-                    <StyledTextInput {...props} />
-                </TouchableOpacity>
-            )}
 
-            {isPassword && (
-                <RightIcon
-                    onPress={() => {
-                        setHidePassword(!hidePassword);
-                    }}
-                >
-                    <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight} />
-                </RightIcon>
-            )}
         </View>
     );
 };
 
-export default SignUp;
+export default MobVerify;
