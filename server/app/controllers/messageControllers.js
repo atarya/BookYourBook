2e75362f8b91d7777835420e60575c2206a5f4b0
Anupam Arya <anu9am@gmail.com>
2022-04-05 20:14:18 +0530

    send message controller added

--- server/app/controllers/messageControllers.js
+++ server/app/controllers/messageControllers.js
@@ -1,6 +1,41 @@
 const expressAsyncHandler = require("express-async-handler");
+const Message = require("../models/Message");
+const Chat = require("../models/Chat");
+const User = require("../models/User");
 
 const sendMessage = expressAsyncHandler(async (req, res) => {
+    const { content, chatId } = req.body;
+    if (!content || !chatId) {
+        console.log("Incorrect content or chatId")
+        return res.status(400)
+    }
+
+    var newMessage = {
+        sender: req.user._id,
+        content: content,
+        chatId: chatId
+    }
+
+    try {
+        var message = await Message.create(newMessage);
+
+        message = await message.populate("sender", "name avatar");
+        message = await message.populate("chat");
+        message = await User.populate(message, {
+            path: "chat.users",
+            select: "name avatar phone"
+        })
+
+        await Chat.findByIdAndUpdate(req.body.chatId, {
+            latestMessage: message,
+        })
+
+        res.json(message);
+
+    } catch (error) {
+        res.status(400)
+        throw new Error(error.message)
+    }
 
 })
 
