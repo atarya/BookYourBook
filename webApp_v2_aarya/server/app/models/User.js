eb9f0b56207b427a07be9d54734d66401aa9030d
Anupam Arya <anu9am@gmail.com>
2022-04-22 01:47:22 +0530

    password encryption pre save

--- webApp_v2_aarya/server/app/models/User.js
+++ webApp_v2_aarya/server/app/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const user = mongoose.Schema(
     {
@@ -88,6 +89,19 @@ const user = mongoose.Schema(
     },
     { timestamps: true }
 );
+
+user.pre('save', async function (next) {
+    try {
+        if (this.isNew) {
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(this.password, salt)
+            this.password = hashedPassword
+        }
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
 
 const User = mongoose.model("User", user);
 
