ff88e431e6f77fe7a71f4c983b0af064ec2751c4
Anupam Arya <anu9am@gmail.com>
2022-04-24 03:33:20 +0530

    books controllers except popular, refining is pending

--- webApp_v2_aarya/server/app/routes/bookRoutes.js
+++ webApp_v2_aarya/server/app/routes/bookRoutes.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const passport = require('passport');
 require("../middlewares/auth")
 const secure = passport.authenticate('jwt', { session: false })
-const { getPopular, getSearch, getSingle, updateSingle, deleteSingle, addSingle } = require("../controllers/bookController");
+const { getPopular, getSearch, getSingle, updateSingle, deleteSingle, addSingle, userList } = require("../controllers/bookController");
 
 // {{URL}}/book/
 // minus books of logged in user and filter? true:false this would filter only the available books 
 router.route("/popular").get(secure, getPopular)
 // search by title, author, genre, check for 3 req.query; (query = search query, user = user._id and filter = true/false this would filter only the available books) minus books of logged in user
 router.route("/search").get(secure, getSearch)
+// get my books
+router.route("/user_list").get(secure, userList)
 // get single book by id
 router.route("/single/:id").get(secure, getSingle)
 // update book by id
