b680c6a2662538b8a2e98f101cf6f226d3f3c47d
vhald <vaibhavgsh@gmail.com>
2022-04-24 14:26:50 +0530

    backend setup

--- server_v/server.js
+++ server_v/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const { readdirSync } = require('fs');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+// db config
+const dbConfig = require('./Config/db')
+dbConfig();
+
+
+//middlewares
+app.use(cors());
+app.use(morgan("dev"));
+
+
 
 // route middleware
 readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
