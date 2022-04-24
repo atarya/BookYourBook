4aaa89386cbdf355229226287113f4eb1421f607
vhald <vaibhavgsh@gmail.com>
2022-04-24 21:54:37 +0530

    axios frontend added

--- client_v/src/auth/Register.js
+++ client_v/src/auth/Register.js
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import RegisterForm from "../components/RegisterForm";
+import axios from "axios";
+
 const Register = () => {
-    return <div className="container-fluid h1 p-5 text-center">Register Page</div>;
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // console.table({ name, email, password });
+        try {
+            const res = await axios.post(`http://localhost:8000/api/register`, {
+                name,
+                email,
+                password,
+            });
+            console.log("REGISTERED USER =>", res);
+        } catch (err) {
+            console.log("ERROR:", err);
+        }
+    };
+
+    return (
+        <>
+            <div className="container-fluid bg-secondary p-5 text-center">
+                <h1>Register</h1>
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <RegisterForm
+                            handleSubmit={handleSubmit}
+                            name={name}
+                            setName={setName}
+                            email={email}
+                            setEmail={setEmail}
+                            password={password}
+                            setPassword={setPassword}
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Register;
