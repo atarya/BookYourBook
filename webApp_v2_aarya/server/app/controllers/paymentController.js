ec4641fa1043d33f3d6cce5c49ac10b07b23688a
Anupam Arya <anu9am@gmail.com>
2022-04-28 11:07:35 +0530

    added razor_pay backend | refinement panding

--- webApp_v2_aarya/server/app/controllers/paymentController.js
+++ webApp_v2_aarya/server/app/controllers/paymentController.js
@@ -2,6 +2,9 @@ const Razorpay = require('razorpay');
 require('dotenv').config({ path: "../../../.env" });
 const { KEY_ID, KEY_SECRET } = process.env;
 const uniqId = require('uniqid');
+const Formidable = require('formidable');
+const { createHmac } = require('crypto');
+let orderId;
 
 var instance = new Razorpay({ key_id: KEY_ID, key_secret: KEY_SECRET });
 
@@ -18,9 +21,24 @@ const createOrder = async (req, res) => {
             res.status(500).json({ message: "Internal Server Error", error: err });
         } else {
             console.log(order);
+            orderId = order.id;
             res.status(200).json({ message: "Order Created", order: order });
         }
     })
 
 }
-module.exports = { createOrder }
+
+const paymentCallback = async (req, res) => {
+    const form = Formidable();
+    form.parse(req, (err, fields, files) => {
+        // console.log(fields);
+        if (fields) {
+            const hash = createHmac("sha256", KEY_SECRET)
+                .update(orderId + "|" + fields.razorpay_payment_id)
+                .digest("hex");
+            console.log("HASH", hash);
+        }
+    })
+}
+
+module.exports = { createOrder, paymentCallback }
--- webApp_v2_aarya/server/app/routes/paymentRoutes.js
+++ webApp_v2_aarya/server/app/routes/paymentRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const router = express.Router()
+const passport = require('passport');
+require("../middlewares/auth")
 const secure = passport.authenticate('jwt', { session: false })
-const { createOrder } = require("../controllers/paymentController")
+const { createOrder, paymentCallback } = require("../controllers/paymentController")
 
+// {{URL}}/payment
 router.route("/createOrder").get(secure, createOrder)
+router.route("/callback").get(secure, paymentCallback)
 
 module.exports = router;
